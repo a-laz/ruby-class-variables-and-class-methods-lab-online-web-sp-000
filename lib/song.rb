93bd1cec656713370f8257f0142c96d28152dc49
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-18 22:48:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,11 @@ class Song
   def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
-      genre_count[genre]
+       if genre_count[genre]
+         genre_count[genre] += 1
+       else
+         genre_count[genre] = 1
+       end
     end
     genre_count
   end
