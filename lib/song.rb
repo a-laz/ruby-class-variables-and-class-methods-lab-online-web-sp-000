16b647f3276b632fe09e36e2c8399ac4fb69a845
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-18 22:30:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,12 @@
 class Song
 
-  def new(name, artist, genre)
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
   end
+  
 
 end
